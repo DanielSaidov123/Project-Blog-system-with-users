d07a959dbe2142ba56e7f6431ff646a5f96054cb
DanielSaidov123 <ddny3622@gmail.com>
2025-12-24 16:30:04 +0200

    add

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -1,4 +1,4 @@
-import { getNextId, readPosts, readUsers, writeUsers } from "../utils/index.js";
+import { getNextId, readPosts, readUsers, writePosts, writeUsers } from "../utils/index.js";
 
 export const GetAllPosts = async (req, res) => {
   try {
@@ -8,4 +8,38 @@ export const GetAllPosts = async (req, res) => {
   }
 };
 
- 
+export const GetPostByID = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const postslist = await readPosts();
+    const post = postslist.find((p) => p.id == id);
+    if (!post) {
+      return res.status(404).send({ msg: "post is not defind." });
+    }
+    res.status(200).send(post);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const CreateUser = async (req, res) => {
+  try {
+    const postslist = await readUsers();
+    if (!req.body.authorId || !req.body.name) {
+      res.status(404).send({ err });
+    }
+    const newpost = {
+      id: getNextId(postslist),
+      title: req.body.title || "",
+      name: req.body.name,
+      content: req.body.content || "",
+      createdAt: new Date(),
+      tags: req.body.tags || [],
+    };
+    postslist.push(newpost);
+    await writePosts(postslist);
+    res.status(200).send(newpost);
+  } catch (err) {
+    res.status(404).send({ err });
+  }
+};
--- routes/Posts.js
+++ routes/Posts.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { } from "../controllers/Users.js"
-import { GetAllPosts } from "../controllers/Posts.js";
+import { GetAllPosts, GetPostByID } from "../controllers/Posts.js";
 const router = express.Router();
 
 
 
 router.route('/').get(GetAllPosts) 
-// router.route('/:id').get(GetUserByID).put(updatingUser).delete(DelStudent)
+router.route('/:id').get(GetPostByID) 
 
 
 export default router;
